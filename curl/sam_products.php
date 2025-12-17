21b8acbc86d8f9c21f54b48ef9fb4c3f2e32a6be
syng20 <samyng20@gmail.com>
2025-12-17 13:01:04 -0800

    htmlspecialchars

--- curl/sam_products.php
+++ curl/sam_products.php
@@ -16,7 +16,7 @@ if (curl_errno($ch)) {
 curl_close($ch);
 
 var_dump($response); 
-echo "JSON Error: " . json_last_error_msg();
+echo "<pre>" . htmlspecialchars($response) . "</pre>";
 
 // Decode JSON to array
 $data = json_decode($response, true);
