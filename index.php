e89f934a832f19978517ed7e8ac37e901857483f
syng20 <samyng20@gmail.com>
2025-12-16 20:12:00 -0800

    start

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title></title>
+   <link href="styles.css" rel="stylesheet">
+</head>
+<body>
+    <header> 
+        <h1>Marketplace</h1>
+    </header>
+    
+    <!-- box selection -->
+    <div class="buttonholder">
+    <a href="index.php"><button class="button"><h2>New User</h2></button></a>
+    </div>
+    <main id="homebuttoncontainer">
+      <div class="buttonholder">
+        <a href="index.php"><button class="button"><h2>M1</h2></button></a>
+      </div>
+      <div class="buttonholder">
+        <a href="index.php"><button class="button"><h2>M2</h2></button></a>
+      </div>
+      <div class="buttonholder">
+        <a href="index.php"><button class="button"><h2>M3</h2></button></a>
+      </div>
+      <div class="buttonholder">
+        <a href="index.php"><button class="button"><h2>M4</h2></button></a>
+      </div>
+  </main>
+
+  <footer>
+      <p>Ver 1.1.0</p>
+  </footer>
+
+</body>
+</html>
